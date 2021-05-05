805eac9560f5f47fcadadcd29075f5f47d437508
Toby <agentalliancela33@gmail.com>
2021-05-04 18:11:08 -0700

    agony

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,10 +1,18 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+type Thought {
+  _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+}
+type query{
+  thoughts : [Thought]
   }
 `;
 
